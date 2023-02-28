050d5e6aeed5056239491e7078c13d2853485558
Devon Neill <devneill@hey.com>
2023-02-28 16:26:47 +0100

    chore: try to provide commonjs export

---
+++ theme/src/index.cjs..ts
@@ -0,0 +1,19 @@
+import { typography } from '~/typography';
+import { border } from '~/border';
+import { opacity } from '~/opacity';
+import { shadows } from '~/shadows';
+import { color as colors } from '~/colors';
+import { spacing } from '~/spacing';
+import { zIndex } from '~/z-index';
+
+module.exports.theme = {
+  colors: { ...colors },
+  spacing: { ...spacing },
+  extend: {
+    ...typography,
+    ...border,
+    ...opacity,
+    ...shadows,
+    ...zIndex,
+  },
+};
