1ff70a3845bf53981ccadf9cc605684e175237ec
Devon Neill <devneill@hey.com>
2023-02-28 13:06:23 +0100

    chore: add color & spacing tokens to tailwind theme

--- theme/src/index.ts
+++ theme/src/index.ts
@@ -1,19 +1,7 @@
-import { typography } from '~/typography';
-import { colors } from '~/colors';
-import { border } from '~/border';
-import { opacity } from '~/opacity';
-import { shadows } from '~/shadows';
+import { color } from '~/colors';
 import { spacing } from '~/spacing';
-import { zIndex } from '~/z-index';
 
 export const theme = {
-  extend: {
-    ...typography,
-    ...colors,
-    ...border,
-    ...opacity,
-    ...shadows,
-    ...spacing,
-    ...zIndex,
-  },
+  spacing: { ...spacing },
+  colors: { ...color },
 };
