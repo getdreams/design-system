ff55ac5ed955e83e5c4638d12310f64f842c74b5
Devon Neill <devneill@hey.com>
2023-03-03 11:13:31 +0100

    fix: typography tokens

--- tokens/typography/src/index.ts
+++ tokens/typography/src/index.ts
@@ -1,10 +1,21 @@
 import { fontSize, fontWeight } from './tokens.json';
 
+const compoundFontSizes = () => {
+  return Object.keys(fontSize).reduce((compoundFontSizes, key) => {
+    compoundFontSizes[key] = [
+      fontSize[key].fontSize,
+      { lineHeight: fontSize[key].lineHeight },
+    ];
+
+    return compoundFontSizes;
+  }, {});
+};
+
 const typography = {
   fontFamily: {
     code: ['Menlo', 'ui-monospace', 'SFMono-Regular', 'Monaco', 'monospace'],
   },
-  fontSize,
+  fontSize: compoundFontSizes(),
   fontWeight,
 };
 
